0a835cf5c4f8fc97218760bd1972704d4bd91119
dchen7321 <danielchen@cmail.carleton.ca>
2016-10-08 12:52:09 -0700

    Add files via upload

---
+++ imagereader.py
@@ -0,0 +1,27 @@
+import os
+from PIL import Image, ImageEnhance, ImageFilter
+import pytesseract
+
+def receipt_to_text(filename)
+	path = filename
+	img = Image.open(path)
+	img = img.convert('RGBA')
+	pix = img.load()
+	# modify image to maximize the readability of text
+	for y in range(img.size[1]):
+		for x in range(img.size[0]):
+			if pix[x, y][0] < 175 or pix[x, y][1] < 175 or pix[x, y][2] < 175:
+				pix[x, y] = (0, 0, 0, 255)
+			else:
+				pix[x, y] = (255, 255, 255, 255)
+	img.save('temp.jpg')
+	im = Image.open("temp3.jpg")
+	im = im.filter(ImageFilter.MedianFilter())
+	enhancer = ImageEnhance.Contrast(im)
+	im = enhancer.enhance(2)
+	im = im.convert('1')
+	im.save('temp.jpg')
+
+	text = pytesseract.image_to_string(Image.open('temp4.jpg'))
+	os.remove('temp.jpg')
+	print(text)
